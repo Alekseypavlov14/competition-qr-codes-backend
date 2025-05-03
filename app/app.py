387fcb8064fb32e5b49cca0d8f3fe74ee5b33289
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-05-03 20:59:31 +0300

    added cors

--- app/app.py
+++ app/app.py
@@ -1,14 +1,21 @@
 from werkzeug.exceptions import HTTPException
 from flask import Flask, json
+from flask_cors import CORS
 from db import db
 import config
 import api
 
+# create app
 app = Flask(__name__)
 
+# add db data
 app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# setup cors
+CORS(app)
+
+# init app
 db.init_app(app)
 app.register_blueprint(api.router) 
 
